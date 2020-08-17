e6e1328e904bde8c43f674dcc854b62f348360a2
onwuasoanya_george <ndu4george@gmail.com>
2020-08-17 14:14:16 +0100

    initial commit for portfolio app

---
+++ dist/js/main.js
@@ -0,0 +1,26 @@
+const menuButton = document.querySelector('.menu-btn');
+const hamburger = document.querySelector('.menu-btn__burger');
+const nav = document.querySelector('.nav');
+const menuNav = document.querySelector('.menu-nav');
+const navItems = document.querySelectorAll('.menu-nav__item');
+
+let showMenu = false;
+menuButton.addEventListener('click', toggleMenu);
+
+
+function toggleMenu() {
+    if (!showMenu) {
+        hamburger.classList.add('open');
+        nav.classList.add('open');
+        menuNav.classList.add('open');
+        navItems.forEach(element => element.classList.add('open'));
+
+        showMenu = true;
+    } else {
+        hamburger.classList.remove('open');
+        nav.classList.remove('open');
+        menuNav.classList.remove('open');
+        navItems.forEach(element => element.classList.remove('open'));
+        showMenu = false;
+    }
+}
